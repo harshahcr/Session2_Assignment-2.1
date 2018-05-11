912a1fa2f1bd7a486a32fa995b1cab7302893730
harshahcr <39063531+harshahcr@users.noreply.github.com>
2018-05-11 18:29:13 +0530

    Add files via upload

---
+++ Assignement 2.1_session_2 -list container.py
@@ -0,0 +1,41 @@
+#List - container programs
+
+#Python string is also a sequence of characters
+strlist = list('Harsha')
+print('The output is sequence of comma seperated alphabets as: ', strlist)
+
+#sequence of comma-separated numbers from console and generate a list.
+number = (11,22,33,44,55,66,77)
+print('Number sequence only',number)
+
+numberlist = list(number)
+print('Generate List:',numberlist)
+
+#Simple print on list 
+var = ['a','b','c','d']
+varlist = list(var)
+print(list(varlist))
+print(var)
+
+#index concepts
+l = []
+l.append('H')
+l.append('a')
+l.append('r')
+l.append('s')
+l.append('h')
+l.append('a')
+print(l)
+      
+#insert content to list
+
+il=[]
+il.append('a')
+il.append('r')
+il.append('h')
+il.append('a')
+il.insert(0,'H')
+il.insert(3,'s')
+print('Alphabets are appended in list to spell my name',il)
+
+
